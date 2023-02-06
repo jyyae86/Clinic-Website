fed3c51e13b886d6a2570029b4931e3ccfae9f62
Yiyang Jiang <jyyae86@gmail.com>
2023-02-06 15:03:29 -0800

    Updates

---
+++ precache-manifest.385863db42d76ca7ba0d5fd16c62e19f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f9e84812f2073201711",
+    "url": "/mywebsite/static/css/main.34fce7bf.chunk.css"
+  },
+  {
+    "revision": "5f9e84812f2073201711",
+    "url": "/mywebsite/static/js/main.5f9e8481.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/mywebsite/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/mywebsite/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "4986fa352715b3243f60",
+    "url": "/mywebsite/static/js/runtime~main.4986fa35.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/mywebsite/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b206af337e77c61e761c9a0f24a53a93",
+    "url": "/mywebsite/index.html"
+  }
+];
